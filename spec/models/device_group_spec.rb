a6d6e7b8ae881b8916bf3b8d717e3a4ba4b251df
slvk14 <slavikslavik1414@gmail.com>
2019-04-22 22:07:00 +0300

    Renamed attribute 'organisation_id' to 'organization_id'. Fixed some problems in models and added simple tests on validations.

--- spec/models/device_group_spec.rb
+++ spec/models/device_group_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe DeviceGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(name: "Anything", organization_id: "1")
+  }
+
+
+   it "is not valid without name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without oraganization_id" do
+    subject.organization_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with name, shorter than 3 symbols" do
+    subject.name = "AB"
+    expect(subject).to_not be_valid
+  end
+
+  
+
 end
