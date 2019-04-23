5e0ed73dab9fcb9abdb17af28d73a26719618aff
misha_makogin <makoginm1998@gmail.com>
2019-04-23 09:57:07 +0300

    CRUD for user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def index
+  	@user = User.all
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+    user = User.create(user_params)
+
+    redirect_to users_path
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, 
+    	                         :password_confirmation, :admin, :organisation_id)
+  end
+end
