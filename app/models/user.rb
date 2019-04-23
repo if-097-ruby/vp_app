ad00e39b3f129ae992af2362bbbbbd39f3ef3337
misha_makogin <makoginm1998@gmail.com>
2019-04-23 09:31:49 +0300

    add test for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
-	  validates :first_name, :last_name,  presence: true, length: { in 3..40 }
+	  validates :first_name, :last_name,  presence: true, length: { maximum: 60 }
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, length: { in 6..255 },
+  	validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },	
                     uniqueness: true
   	validates :password, :presence => true,
