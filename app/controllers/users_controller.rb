f211c32f531db061da601983b85a6129a53e7ed6
misha_makogin <49226425+misyo98@users.noreply.github.com>
2019-04-23 16:13:57 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
-    private
+private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, 
