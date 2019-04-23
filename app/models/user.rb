d4c42a4da40a55b1e00479578fba448858d6b5a9
misha_makogin <49226425+misyo98@users.noreply.github.com>
2019-04-23 11:20:01 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
 
-	  validates :first_name, :last_name,  presence: true, length: { maximum: 60 }
-  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },	
-                    uniqueness: true
-  	validates :password, :presence => true,
-                         :confirmation => true,
-                         :length => {:within => 6..40},
-                         :on => :create
-  	validates :password, :confirmation => true,
-                         :length => {:within => 6..40},
-                         :allow_blank => true,
-                         :on => :update
-  	has_one :organisation_id
+  validates :first_name, :last_name,  presence: true, length: { maximum: 60 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+             format: { with: VALID_EMAIL_REGEX },	
+             uniqueness: true
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+  validates :password, :confirmation => true,
+                       :length => {:within => 6..40},
+                       :allow_blank => true,
+                       :on => :update
+  has_one :organisation_id
 
 end
