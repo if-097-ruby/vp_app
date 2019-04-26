ee885c5d6afd93042655c463a5b010174e1e6455
Yurii <hrytsykyur@gmail.com>
2019-04-26 13:45:37 +0300

    fix

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   has_many :users, dependent: :destroy
-  has many :device_groups
+  has_many :device_groups
 end
