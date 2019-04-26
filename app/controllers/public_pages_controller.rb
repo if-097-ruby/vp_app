22c69721ee8e4c00ec855a55232c6a2d6fc5c65f
Yulia Cheliak <bluetownbird@gmail.com>
2019-04-26 21:25:59 +0300

    Add sticky footer

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -1,4 +1,7 @@
 class PublicPagesController < ApplicationController
   def home
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'public_pages#home'
+  get  'public_pages/create'
 
   resources :contacts, only: [:new, :create]
 
