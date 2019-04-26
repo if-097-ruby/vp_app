a5b4da7db818e829c2be6ce7fed06397eb79ee50
Pavlo Semehen <semegen2010@gmail.com>
2019-04-26 11:43:03 +0300

    fix migrations, user model, add password validation

--- db/migrate/20190425150753_add_references_organization_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddReferencesOrganizationIdToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :users, :organization, foreign_key: true
-  end
-end
--- db/migrate/20190426064050_add_reference_organization_id_to_device_group.rb
+++
@@ -1,6 +0,0 @@
-class AddReferenceOrganizationIdToDeviceGroup < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :device_groups, :organization, foreign_key: true
-
-  end
-end
