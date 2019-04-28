24e8b826fdd231406a180033a5a1b29742ae7637
Pavlo Semehen <semegen2010@gmail.com>
2019-04-28 18:01:17 +0300

    Add users_controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,12 @@
-=begin
 require 'rails_helper'
 
 RSpec.describe Account::UsersController, type: :controller do
+  let!(:user) { build(:user) }
 
+  describe "GET index" do
+    it "should get index" do
+      get 'index'
+      response.should be_success
+    end
   end
 end
-=end
