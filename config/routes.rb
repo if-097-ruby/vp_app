2c9c022391c7f8ca9cd8f696dd5f5d03b6f73f81
Pavlo Semehen <semegen2010@gmail.com>
2019-05-09 16:45:57 +0300

    change routes to /login & /sign_up, change organization name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
+             controllers: { registrations: 'users/registrations' }
 
   namespace :account do
     root to: 'dashboard#index'
