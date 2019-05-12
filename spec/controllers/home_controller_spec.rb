f87152c729db33b5ebee2e9fcd2ac5718844ee3e
slvk14 <slavikslavik1414@gmail.com>
2019-05-12 15:08:17 +0300

    Simple spec added

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -17,4 +17,11 @@ RSpec.describe HomeController, type: :controller do
     end
   end
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
