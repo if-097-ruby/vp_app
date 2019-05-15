d703762c765f897aaea1d18218f80aab55a6ff74
misha_makogin <makoginm1998@gmail.com>
2019-05-15 21:51:16 +0300

    add user show page

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -5,21 +5,4 @@ class Account::ProfilesController < ApplicationController
   	@user = current_user.id
   end
 
-  def edit
-      @user = current_user.id
-  end
-
-  def update
-      @profile = current_user.profile
-    respond_to do |format|
-      if @profile.update profile_params
-        format.html { redirect_to edit_profile_path, notice: "Profile updated!" }
-        format.json { render :edit, status: :ok, location: @profile }
-      else
-        format.html { redirect_to edit_profile_path, flash: { error: "Profile could not be updated!" } }
-        format.json { render json: @profile.errors.messages, status: :unprocessable_entity }
-      end
-    end
-  end
-
 end
