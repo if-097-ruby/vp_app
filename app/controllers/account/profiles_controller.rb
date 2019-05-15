5f26cf2dc8f70aaa849c53e386551db3b10fd4a3
misha_makogin <makoginm1998@gmail.com>
2019-05-15 23:36:31 +0300

    some changes with edit functional

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -5,4 +5,16 @@ class Account::ProfilesController < ApplicationController
   	@user = current_user.id
   end
 
+  def edit
+  	@user = current_user.id
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      redirect_to account_profile_path, :flash => { :success => "Profile updated." }
+    else
+      render 'edit'
+    end
+  end
+
 end
