d33322ecef485d3cd4c80ee675b3fdfe0f8442aa
vikaluk <vikaluk@gmail.com>
2019-05-16 20:46:34 +0300

    Small changes

--- spec/models/contact_validation_spec.rb
+++ spec/models/contact_validation_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe Contact, type: :model do
   context 'validation tests' do
     let!(:contact) { create(:contact) }
 
-  describe 'factory' do
-    it { is_expected.to be_truthy }
-  end
+    describe 'factory' do
+      it { is_expected.to be_truthy }
+    end
 
-  describe '#name' do
-    it { is_expected.to validate_presence_of(:name) }
-  end
+    describe '#name' do
+      it { is_expected.to validate_presence_of(:name) }
+    end
 
-  describe '#message' do
-    it { is_expected.to validate_presence_of(:message) }
-  end
+    describe '#message' do
+      it { is_expected.to validate_presence_of(:message) }
+    end
 
-  describe '#email' do
-    it { expect(contact).to_not allow_value("base@example").for(:email) }
-    it { expect(contact).to allow_value("dhh@nonopinionated.com").for(:email) }
-    it { is_expected.to validate_presence_of(:email) }
+    describe '#email' do
+      it { expect(contact).to_not allow_value("base@example").for(:email) }
+      it { expect(contact).to allow_value("dhh@nonopinionated.com").for(:email) }
+      it { is_expected.to validate_presence_of(:email) }
+    end
   end
-end
 end
