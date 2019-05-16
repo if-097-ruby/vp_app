3543a9c326a2c9d0b3c662638b0aa3ceb11c9510
slvk14 <slavikslavik1414@gmail.com>
2019-05-16 19:46:36 +0300

    Pricing page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/about', to: 'home#about'
+  get '/pricing', to: 'home#pricing'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
              controllers: { registrations: 'users/registrations' }
