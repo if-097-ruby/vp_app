6063da066ec3691e4ac2073439b81f8358f58d3e
vikaluk <vikaluk@gmail.com>
2019-05-29 11:47:14 +0300

    Fix sma;; issues

--- app/models/device_group.rb
+++ app/models/device_group.rb
@@ -1,5 +1,4 @@
 class DeviceGroup < ApplicationRecord
-  has_many :devices, dependent: :destroy
   belongs_to :organization
 
   validates :name, presence: true, length: { in: 3..20 }
--- db/migrate/20190527193410_add_device_id.rb
+++
@@ -1,5 +0,0 @@
-class AddDeviceId < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :devices, :device_group, foreign_key: true
-  end
-end
