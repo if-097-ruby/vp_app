29adf801a8013899a475a9371f6980a8d723024d
slvk14 <slavikslavik1414@gmail.com>
2019-06-09 17:09:07 +0300

    deleted unneeded migrations

--- db/migrate/20190605150126_change_default_user_role.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultUserRole < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:users, :role, from: 0, to: 1)
-  end
-end
--- db/migrate/20190606194808_change_default_user_role_back.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultUserRoleBack < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:users, :role, from: 1, to: 0)
-  end
-end
